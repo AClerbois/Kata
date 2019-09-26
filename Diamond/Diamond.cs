d754d1ed6db22c064dd6d430c583b654304be56c
Adrien Clerbois <50712277+AClerbois@users.noreply.github.com>
2019-09-26 08:53:00 +0200

    Update Diamond.cs

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -6,11 +6,13 @@ namespace AClerbois.Kata
 	{
 		public static void Main()
 		{
-			var n = 9;
+			var maxRange = 9;
+			var n = 6;
 			for (int i = 1; i <= (n * 2); i++)
 			{
 				var rest = i < n ? n - i : i - n;
-				Console.Write(new string (' ', rest));
+				var white = maxRange - n + rest;
+				Console.Write(new string (' ', white));
 				for (int j = 1; j <= n - rest; Console.Write(j++))
 					;
 				for (int k = n - rest - 1; k >= 1; Console.Write(k--))
